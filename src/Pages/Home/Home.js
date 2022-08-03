4f009d515c3c8a5d900824f54e6d46fc65a9a478
Pyradio <aditya.a@polynomial.ai>
2022-08-03 06:23:52 +0530

    Revert "again:url shortner"
    This reverts commit 55bd57a05f38328e081b29fb9c96d1949b8d6e2c.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import ConfirmationModal from "../../components/ConfirmationModal/ConfirmationModal";
 import FileUploader from "../../components/FileUploader/FileUploader";
@@ -27,8 +26,9 @@ const Home = () => {
     try {
       setIsLoading(true);
       const response = await axios.post(
-        `https://consise-farms.herokuapp.com/admin/generateUrl`,
-        data
+        `https://20f5-117-207-8-159.in.ngrok.io/upload`,
+        data,
+        {responseType: 'blob'}
       );
       // console.log("Response", response);
       var a = document.createElement("a");
@@ -40,7 +40,7 @@ const Home = () => {
       setUrl(response?.data?.URL);
       setMessage({
         heading: "Success!",
-        text: `Congraulations! Your file/files have been successfully uploaded to your dashboard.`,
+        text: `Congratulations! Your file/files have been successfully uploaded to your dashboard.`,
       });
       setIsLoading(false);
       setEnteredText("");
@@ -80,33 +80,26 @@ const Home = () => {
   };
   return (
     <div className="flex flex-col items-center justify-center space-y-10 shadow-lg my-10 max-w-3xl p-10 mx-auto">
-      <div
-        style={{ minHeight: "15rem" }}
-        className="border-2 border-gray-300 w-full rounded-md p-5"
-      >
-        <textarea
-          placeholder={
-            files.length === 0
-              ? "Input or paste code snippets"
-              : "You can either enter text or upload a file"
-          }
-          style={{ resize: "none" }}
+      {/* <div className="border-2 border-gray-300 w-full rounded-md py-3 px-5">
+        <input
+          placeholder="Enter YouTube video link"
           className="w-full focus:outline-none bg-transparent"
           value={enteredText}
           onChange={(e) => setEnteredText(e.target.value)}
           name="code-snippets"
-          disabled={files.length > 0}
         />
-      </div>
-      <div>
+      </div> */}
+
+      {<div>
         <FileUploader
           files={files}
           setFiles={setFiles}
           enteredText={enteredText}
           placeholder="Select Image to be segmented"
         />
-      </div>
-      <div className="border-2 border-gray-300 rounded-md w-80 px-8 py-3">
+      </div> }
+
+      <div className="form-check">
         <input
           id="checkbox"
           className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
